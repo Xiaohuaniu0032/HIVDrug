694f8c275e6c7654056247b9775356fced4385a2
longfei.fu <longfei.fu@thermofisher.com>
2022-06-23 11:51:44 +0800

    update

--- get_drug_DNA_and_AA.pl
+++ get_drug_DNA_and_AA.pl
@@ -1,13 +1,94 @@
 use strict;
 use warnings;
+use File::Basename;
+use FindBin qw/$Bin/;
+
+my ($outdir) = @ARGV;
+
+my $ref = "$Bin/ref/K03455.fasta";
+my $samtools = "/usr/bin/samtools";
+
+my %codon_aa;
+my $codon_file = "$Bin/codon.list";
+
+open IN, "$codon_file" or die;
+while (<IN>){
+	chomp;
+	next if /^\#/;
+	next if /^$/;
+	next if /^Amino/; # skip header
+	# Isoleucine      I       ATT, ATC, ATA
+	my @arr = split /\t/;
+	my $aa  = "$arr[1]\t$arr[0]";
+	my $dna = $arr[2];
+	my @dna = split /\,/, $dna;
+	for my $dna (@dna){
+		$dna =~ s/^\s+//;
+		$codon_aa{$dna} = $aa; # AAT => "I \t Isoleucine"
+	}
+}
+close IN;
 
 
-my ($ref) = @ARGV;
+my %AA_Pos;
+$AA_Pos{"Protease"} = "2253,2549";
+$AA_Pos{"RT"}       = "2550,4229";
+$AA_Pos{"Integrase"} = "4233,5096";
+
+my @AA = qw/Protease RT Integrase/;
+
+for my $aa (@AA){
+	my $pos = $AA_Pos{$aa};
+	my @pos = split /\,/, $pos;
+	my $sp = $pos[0];
+	my $ep = $pos[1];
+
+	my $outfile = "$outdir/$aa\.$sp\.$ep\.fasta";
+	my $cmd = "$samtools faidx $ref K03455.1\:$sp\-$ep >$outfile";
+	system($cmd);
+
+	my $seq = cat_fa($outfile);
+	#print "$seq\n";
+	my $len = length($seq);
+
+	my $idx = 0;
+	for (my $i=0; $i<=$len-3; $i+=3){
+		$idx += 1;
+		my $codon = substr($seq,$i,3);
+		
+		my $val;
+		if (exists $codon_aa{$codon}){
+			$val = $codon_aa{$codon};
+		}else{
+			$val = "NA";
+		}
+		
+		print "$aa\t$idx\t$codon\t$val\n";
+	}
+
+}
+
+
+sub cat_fa{
+	my ($fa) = @_;
+	my @seq;
+	open IN, "$fa" or die;
+	while (<IN>){
+		chomp;
+		next if /^\>/;
+		next if /^$/;
+		push @seq, $_;
+	}
+	close IN;
+	my $seq = join("",@seq);
+
+	return($seq);
+}
 
 # Ref is K03455.1
 # CDS [2358..5096] [pol polyprotein] [codon_start=1]
 
-# Protease                  [2253,2357]
+# Protease                  [2253,2549]
 # Reverse Transcriptase     [2550,4229]
 # Integrase                 [4233,5096]
 
